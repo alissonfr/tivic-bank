b0a14b98ae83362827bed8e04d0bd13452b824ab
Alisson Rodrigues <alissonrodrigues565656@gmail.com>
2023-03-31 00:56:55 -0300

    feat(backend): setup swagger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import routes from './routes/routes'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocs from './swagger.json'
 
 const app = express()
 app.use(express.json({ limit: '50mb' }))
 app.use(cors())
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(routes)
 
 export { app }
